55bab8a3e3c503e37920f696a1c6c52bf58ae3fd
Roman Kulikov <roman.kulikov.ufa@gmail.com>
2017-05-19 09:13:17 +0300

    Added todo-list for the future

---
+++ src/main/java/com/rok/sorter/ToDO.java
@@ -0,0 +1,11 @@
+package com.rok.sorter;
+
+/**
+ * Created by roman.kulikov on 5/19/2017.
+ * All rights reserved =D
+ */
+public class ToDO {
+    //todo: Change SQLite db engine to H2/HSQL/Derby
+    //todo: Add Spring MVC Mock integrational tests
+    //todo: Revise algorithm and see if Java 8 techniques might be used
+}
